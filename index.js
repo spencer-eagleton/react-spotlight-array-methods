018ebee4fa56ca7dae194d9580c11ec2b401cf81
Spencer Eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-12-07 10:11:55 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// INPUT: the array of countries from data.js
+// OUTPUT: an array of the names
+// REQS: use .map
+
+export const getCountryNames = (countries) => {};
+
+// INPUT: the array of countries from data.js
+// OUTPUT: an array of the names and their iso2 country codes using .map
+// REQS: use .map
+
+export const getCountryCodes = (countries) => {};
+
+// INPUT: the array of countries from data.js
+// OUTPUT: only countries in Oceana
+// REQS: use .filter
+
+export const filterOceania = (countries) => {};
+
+// INPUT: the array of countries from data.js
+// OUTPUT: only countries that include the letters 'au'
+// REQS: use .filter
+
+export const filterAu = (countries) => {};
+
+// INPUT: the array of brands from data.js
+// OUTPUT: an array of all the sneakers brands
+// REQS: use .reduce
+
+export const getSneaks = (brands) => {};
---
+++ test/index.test.js
@@ -0,0 +1,96 @@
+import { filterAu, filterOceania, getCountryCodes, getCountryNames, getSneaks } from '..';
+import { brands, countries } from '../data';
+
+test('getCountryNames should return the country names', () => {
+  expect(getCountryNames(countries)).toStrictEqual([
+    'Ecuador',
+    'Lesotho',
+    'Yemen',
+    'Saudi Arabia',
+    'Saint Kitts and Nevis',
+    'Sierra Leone',
+    'Saint Vincent and the Grenadines',
+    'United Kingdom',
+    'Jersey',
+    'Gambia',
+    'Kiribati',
+    'Sri Lanka',
+    'Burkina Faso',
+    'Sao Tome and Principe',
+    'Cape Verde',
+    'Andorra',
+    'Martinique',
+    'Malaysia',
+    'Mauritania',
+    'Japan',
+    'Heard Island and Mcdonald Islands',
+    'Estonia',
+    'Gibraltar',
+    'Marshall Islands',
+    'French Polynesia',
+    'Bosnia and Herzegovina',
+    'Marshall Islands',
+  ]);
+});
+
+test.skip('getCountryCodes return a nested array of countries and their codes', () => {
+  expect(getCountryCodes(countries)).toStrictEqual([
+    ['Ecuador', 'EC'],
+    ['Lesotho', 'LS'],
+    ['Yemen', 'YE'],
+    ['Saudi Arabia', 'SA'],
+    ['Saint Kitts and Nevis', 'KN'],
+    ['Sierra Leone', 'SL'],
+    ['Saint Vincent and the Grenadines', 'VC'],
+    ['United Kingdom', 'GB'],
+    ['Jersey', 'JE'],
+    ['Gambia', 'GM'],
+    ['Kiribati', 'KI'],
+    ['Sri Lanka', 'LK'],
+    ['Burkina Faso', 'BF'],
+    ['Sao Tome and Principe', 'ST'],
+    ['Cape Verde', 'CV'],
+    ['Andorra', 'AD'],
+    ['Martinique', 'MQ'],
+    ['Malaysia', 'MY'],
+    ['Mauritania', 'MR'],
+    ['Japan', 'JP'],
+    ['Heard Island and Mcdonald Islands', 'HM'],
+    ['Estonia', 'EE'],
+    ['Gibraltar', 'GI'],
+    ['Marshall Islands', 'MH'],
+    ['French Polynesia', 'PF'],
+    ['Bosnia and Herzegovina', 'BA'],
+    ['Marshall Islands', 'MH'],
+  ]);
+});
+
+test.skip('filterOceania returns countries in Oceania', () => {
+  expect(filterOceania(countries).map((elem) => elem.iso2)).toStrictEqual(['KI', 'MH', 'PF', 'MH']);
+});
+
+test.skip('filterAu returns countries that contain au', () => {
+  expect(filterAu(countries).map((elem) => elem.name)).toStrictEqual([
+    'Saudi Arabia',
+    'Mauritania',
+  ]);
+});
+
+test.skip('getSneaks returns the list of sneaker brands', () => {
+  expect(getSneaks(brands)).toStrictEqual([
+    'Air Max',
+    'Air Force',
+    'Air Jordan',
+    'Air Mag',
+    'Pegasus',
+    'Kobe',
+    'Clyde',
+    'Suede',
+    'Roma',
+    'California',
+    'Ultraboost',
+    'Gazelle',
+    'Samba',
+    'Superstar',
+  ]);
+});
